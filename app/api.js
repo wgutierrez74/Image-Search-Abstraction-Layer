2ca695075f1473a2679a9f6eb18f9dda19fb2a4c
System <none>
2017-07-28 20:21:17 +0000

    Checkpoint

--- app/api.js
+++ app/api.js
@@ -12,7 +12,7 @@ var bl = require('bl');
 var methods = {};
 
 
-methods.pixabay = function(search){
+methods.pixabay = function(search, response){
   var url = "https://pixabay.com/api/?key=6024228-33f0d0b2607fe85665a765b2a&q="+search+"&image_type=photo";
   // return $.getJSON(url, function(data){
   //     return data;
@@ -24,17 +24,16 @@ methods.pixabay = function(search){
     method: "GET"
   };
   
-  var getRequest = https.request(options, function(res){
+  https.request(options, function(res){
       res.setEncoding("utf8");
       res.pipe(bl(function(err, data){
         if(err){console.log(err); return;}
         //console.log(data);
         //console.log(data.toString().length);
         console.log(data.toString());
-        res.write(data.toString());
-  }))});
+        response.send(data.toString());
+  }))}).end();
   
-  getRequest.end();
   
 }
   //return data
--- app/routes.js
+++ app/routes.js
@@ -18,8 +18,8 @@ module.exports = function(app){
     
     
     var offset = req.query.offset;
-    consume(search);
-    
+    //consume(search);
+    var pixabay = api.pixabay(search, res);
     //res.send(pixabay);
     
     
@@ -34,33 +34,33 @@ module.exports = function(app){
   });
   
   
-  function consume(search){
-      var url = "https://pixabay.com/api/?key=6024228-33f0d0b2607fe85665a765b2a&q="+search+"&image_type=photo";
-  // return $.getJSON(url, function(data){
-  //     return data;
-  // });
-  var options = {
-    host : "pixabay.com",
-    port: 443,
-    path: "/api/?key=6024228-33f0d0b2607fe85665a765b2a&q="+search+"&image_type=photo",
-    method: "GET"
-  };
+//   function consume(search){
+//       var url = "https://pixabay.com/api/?key=6024228-33f0d0b2607fe85665a765b2a&q="+search+"&image_type=photo";
+//   // return $.getJSON(url, function(data){
+//   //     return data;
+//   // });
+//   var options = {
+//     host : "pixabay.com",
+//     port: 443,
+//     path: "/api/?key=6024228-33f0d0b2607fe85665a765b2a&q="+search+"&image_type=photo",
+//     method: "GET"
+//   };
   
-  var getRequest = https.request(options, function(res){
-      res.setEncoding("utf8");
-      res.pipe(bl(function(err, data){
-        if(err){console.log(err); return;}
-        //console.log(data);
-        //console.log(data.toString().length);
-        console.log(data.toString());
-        res.write(data.toString());
-  }))});
+//   var getRequest = https.request(options, function(res){
+//       res.setEncoding("utf8");
+//       res.pipe(bl(function(err, data){
+//         if(err){console.log(err); return;}
+//         //console.log(data);
+//         //console.log(data.toString().length);
+//         console.log(data.toString());
+//         res.write(data.toString());
+//   }))});
   
-  getRequest.end();
+//   getRequest.end();
     
     
     
-  }
+//   }
   
   
   
