8591aa18622f88aa99c0dac43a61c91fe7e8f60a
System <none>
2017-07-28 19:26:17 +0000

    Checkpoint

--- app/api.js
+++ app/api.js
@@ -1,17 +1,37 @@
-var $ = require('jquery').create();
+// let jsdom = require("jsdom");
+// let $ = null;
+
+// jsdom.env("http://quaintous.com/2015/07/31/jquery-node-mystery/",
+//   function (err, window) {
+//     $ = require('jQuery')(window);
+//   }
+// );
+var https = require('https');
 
 var methods = {};
 
 
 methods.pixabay = function(search){
   var url = "https://pixabay.com/api/?key=6024228-33f0d0b2607fe85665a765b2a&q="+search+"&image_type=photo";
-  return $.getJSON(url, function(data){
-      return data;
-  });
+  // return $.getJSON(url, function(data){
+  //     return data;
+  // });
+  var options = {
+    host : "pixabay.com",
+    port: 443,
+    path: "api/?key=6024228-33f0d0b2607fe85665a765b2a&q="+search+"&image_type=photo",
+    method: "GET"
+  };
   
+  var getRequest = https.request(options, function(res){
+      res.on('data', function(data){
+        return data;
+      })
+  });;
   
-  
+  getRequest.end();
   
   
   //return data;
 }
+ module.exports=methods;
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,7 @@
 var db = require("./database.js");
 var api = require("./api.js");
 
-
 module.exports = function(app){
-  
   
   app.route('/').get(function(req, res) {
 		  res.sendFile(process.cwd() + '/views/index.html');
