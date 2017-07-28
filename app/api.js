92e7d85927f929c3927320fd13a08356984239f5
System <none>
2017-07-28 18:42:16 +0000

    Checkpoint

--- app/api.js
+++ app/api.js
@@ -1,11 +1,13 @@
-
+var $ = require("jquery");
 
 var methods = {};
 
 
 methods.pixabay = function(search){
-  
-  
+  var url = "https://pixabay.com/api/?key=6024228-33f0d0b2607fe85665a765b2a&q="+searchyellow+flowers&image_type=photo"
+  return $.getJSON(url, function(data){
+      
+  });
   
   
   
